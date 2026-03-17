83df025590314ee91aca6b2b3e724d9591d8cbc0
sortascary <66732198+sortascary@users.noreply.github.com>
2026-03-17 10:43:42 +0000

    [DZ] Fix: css adn js files config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,30 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/home.css',
+                'resources/css/client.css',
+                'resources/css/contact.css',
+                'resources/css/services.css',
+                'resources/css/about.css',
+                'resources/css/chat.css',
+                'resources/css/dashboard.css',
+                'resources/js/about.js',
+                'resources/js/app.js',
+                'resources/js/blog.js',
+                'resources/js/chart.js',
+                'resources/js/chat.js',
+                'resources/js/client.js',
+                'resources/js/DashForum.js',
+                'resources/js/echo.js',
+                'resources/js/forms.js',
+                'resources/js/hero.js',
+                'resources/js/home.js',
+                'resources/js/navbar.js',
+                'resources/js/notification.js',
+                'resources/js/services.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
